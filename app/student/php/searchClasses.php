144ab30f0b0df9191bc9e96e3e26186d290c159e
Paul McGurk <pmcgurk@users.noreply.github.com>
2016-02-05 21:11:33 +0000

    searchClasses.php now filters enrolled classes

--- app/student/php/searchClasses.php
+++ app/student/php/searchClasses.php
@@ -2,12 +2,19 @@
 
 include("../php/connect.php");
 $value = $_GET['value'];
- $result = mysql_query("SELECT classes.cid, classes.name, classes.code, classes.description, classes.isvisible, owned.uid, users.realname FROM (enrolled) INNER JOIN (classes) ON (enrolled.cid = classes.cid) INNER JOIN (owned) ON (classes.cid = owned.cid) INNER JOIN (users) ON (owned.uid = users.uid) WHERE classes.code LIKE '%$value%'");
- $rows = array();
-   while($r = mysql_fetch_assoc($result)) {
-        $rows[] = $r;
-   }
+session_start();
+$uid    = $_SESSION['id'];
+$result = mysql_query("SELECT classes.cid, classes.name, classes.code, classes.description, classes.isvisible, owned.uid, users.realname FROM (classes) INNER JOIN (owned) ON (classes.cid = owned.cid) INNER JOIN (users) ON (owned.uid = users.uid) WHERE classes.code LIKE '%$value%'");
 
- print json_encode($rows);
+$rows = array();
+while ($r = mysql_fetch_assoc($result)) {
+    $cid   = $r['cid'];
+    $count = mysql_fetch_array(mysql_query("SELECT 1 FROM (enrolled) WHERE enrolled.cid = '$cid' AND enrolled.uid = '$uid'"));
+    if ($count[0] == 0) {
+        $rows[] = $r;
+    }
+}
+
+print json_encode($rows);
 
 ?>
