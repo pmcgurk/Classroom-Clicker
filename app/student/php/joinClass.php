748fd89cc33163250e1128f7c87a9e86a07c18bc
Paul McGurk <pmcgurk@users.noreply.github.com>
2016-02-05 21:47:41 +0000

    Added protection for multiple enrol attempts

--- app/student/php/joinClass.php
+++ app/student/php/joinClass.php
@@ -2,6 +2,11 @@
 include("../php/connect.php");
 session_start();
 $uid = $_SESSION['id'];
-mysql_query ("INSERT INTO `clicker`.`enrolled` (`cid`, `uid`) VALUES ('$_GET[cid]', '$uid')") or die(mysql_error());
-echo "success?";
-?> 
+$count = mysql_fetch_array(mysql_query("SELECT 1 FROM (`clicker`.`enrolled`) WHERE enrolled.cid = '$_GET[cid]' AND enrolled.uid = '$uid'"));
+if ($count[0] == 0) {
+    mysql_query ("INSERT INTO `clicker`.`enrolled` (`cid`, `uid`) VALUES ('$_GET[cid]', '$uid')") or die(mysql_error());
+    echo "Successfully Enrolled.";
+} else {
+    echo "Already Enrolled.";
+}
+?>
