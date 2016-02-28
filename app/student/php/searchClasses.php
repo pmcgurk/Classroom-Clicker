a25ec353e548b3eb43acd2da1bdff47519bd1b7f
Paul McGurk <pmcgurk@users.noreply.github.com>
2016-02-28 00:29:10 +0000

    implemented joinable in search

--- app/student/php/searchClasses.php
+++ app/student/php/searchClasses.php
@@ -4,13 +4,13 @@ session_start();
 $uid = $_SESSION['id'];
 if(isset($_GET['code'])) {
     $value = $_GET['code'];
-    $result = mysql_query("SELECT classes.cid, classes.name, classes.code, classes.description, classes.isvisible, owned.uid, users.realname FROM (classes) INNER JOIN (owned) ON (classes.cid = owned.cid) INNER JOIN (users) ON (owned.uid = users.uid) WHERE classes.code LIKE '%$value%'");
+    $result = mysql_query("SELECT classes.cid, classes.name, classes.code, classes.description, classes.isvisible, owned.uid, users.realname FROM (classes) INNER JOIN (owned) ON (classes.cid = owned.cid) INNER JOIN (users) ON (owned.uid = users.uid) WHERE classes.code LIKE '%$value%' AND classes.joinable = 1");
 } else if(isset($_GET['lecturer'])) {
     $value = $_GET['lecturer'];
-    $result = mysql_query("SELECT classes.cid, classes.name, classes.code, classes.description, classes.isvisible, owned.uid, users.realname FROM (classes) INNER JOIN (owned) ON (classes.cid = owned.cid) INNER JOIN (users) ON (owned.uid = users.uid) WHERE users.realname LIKE '%$value%'");
+    $result = mysql_query("SELECT classes.cid, classes.name, classes.code, classes.description, classes.isvisible, owned.uid, users.realname FROM (classes) INNER JOIN (owned) ON (classes.cid = owned.cid) INNER JOIN (users) ON (owned.uid = users.uid) WHERE users.realname LIKE '%$value%' AND classes.joinable = 1");
 } else if(isset($_GET['name'])) {
     $value = $_GET['name'];
-    $result = mysql_query("SELECT classes.cid, classes.name, classes.code, classes.description, classes.isvisible, owned.uid, users.realname FROM (classes) INNER JOIN (owned) ON (classes.cid = owned.cid) INNER JOIN (users) ON (owned.uid = users.uid) WHERE classes.name LIKE '%$value%'");
+    $result = mysql_query("SELECT classes.cid, classes.name, classes.code, classes.description, classes.isvisible, owned.uid, users.realname FROM (classes) INNER JOIN (owned) ON (classes.cid = owned.cid) INNER JOIN (users) ON (owned.uid = users.uid) WHERE classes.name LIKE '%$value%' AND classes.joinable = 1");
 }
 $rows = array();
 while ($r = mysql_fetch_assoc($result)) {
