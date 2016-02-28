4710301bd6029b80a19ba2e9d080043e4a71b858
Paul McGurk <pmcgurk@users.noreply.github.com>
2016-02-28 16:50:18 +0000

    added order by date to get lectures

--- app/lecturer/php/getLectures.php
+++ app/lecturer/php/getLectures.php
@@ -1,6 +1,6 @@
 <?php
 include("connect.php");
-$result = mysql_query("SELECT * FROM lectures WHERE lectures.cid=$_GET[cid]") or die(mysql_error());
+$result = mysql_query("SELECT * FROM lectures WHERE lectures.cid=$_GET[cid] ORDER BY date ASC") or die(mysql_error());
 $rows = array();
 while($r = mysql_fetch_assoc($result)) {
     $rows[] = $r;
