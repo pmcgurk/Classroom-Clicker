7d68ddf73899f245c6d03407a433a1b714dfacc6
Paul McGurk <pmcgurk@users.noreply.github.com>
2016-03-02 14:46:45 +0000

    tidied up php

--- app/student/php/getUser.php
+++ app/student/php/getUser.php
@@ -1,17 +1,11 @@
 <?php
-
 include("connect.php");
 session_start();    
 $uid = $_SESSION['id'];
-if (isset($uid)) {
-    $result = mysql_query("SELECT users.uid, users.isLecturer, users.realname, users.username FROM users WHERE users.uid = {$uid}") or die(mysql_error());
-    $rows = array();
-    while($r = mysql_fetch_assoc($result)) {
-        $rows = $r;
-    }
-    print json_encode($rows);
-} else {
-    echo "null";
+$result = mysql_query("SELECT users.uid, users.isLecturer, users.realname, users.username FROM users WHERE users.uid = {$uid}") or die(mysql_error());
+$rows = array();
+while($r = mysql_fetch_assoc($result)) {
+    $rows = $r;
 }
-
+print json_encode($rows);
 ?>
