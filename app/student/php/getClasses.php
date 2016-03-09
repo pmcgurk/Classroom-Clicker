260ec4b66f5417f085a9e6bf16f392bbc8af8d08
Paul McGurk <pmcgurk@users.noreply.github.com>
2016-03-09 22:20:16 +0000

    tweaked ordering code in php

--- app/student/php/getClasses.php
+++ app/student/php/getClasses.php
@@ -3,7 +3,7 @@
 include("connect.php");
 session_start();    
 $uid = $_SESSION['id'];
-$result = mysql_query("SELECT classes.cid, classes.name, classes.code, classes.description, classes.isvisible, owned.uid, users.realname FROM (enrolled) INNER JOIN (classes) ON (enrolled.cid = classes.cid) INNER JOIN (owned) ON (classes.cid = owned.cid) INNER JOIN (users) ON (owned.uid = users.uid) WHERE enrolled.uid = {$uid}");
+$result = mysql_query("SELECT classes.cid, classes.name, classes.code, classes.description, classes.isvisible, owned.uid, users.realname FROM (enrolled) INNER JOIN (classes) ON (enrolled.cid = classes.cid) INNER JOIN (owned) ON (classes.cid = owned.cid) INNER JOIN (users) ON (owned.uid = users.uid) WHERE enrolled.uid = {$uid} ORDER BY code ASC");
 $rows = array();
 while($r = mysql_fetch_assoc($result)) {
     $rows[] = $r;
