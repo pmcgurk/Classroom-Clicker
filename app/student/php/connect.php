a9f3221c43598e98e1eb67b25bce3dbbd18ae956
Paul McGurk <pmcgurk@users.noreply.github.com>
2016-03-20 16:46:22 +0000

    Changes to connect.php to use new session folder

--- app/student/php/connect.php
+++ app/student/php/connect.php
@@ -1,1 +1,6 @@
-<?php mysql_connect("localhost", "root", ""); mysql_select_db("clicker") or die(mysql_error()); ?>
+<?php 
+    mysql_connect("localhost", "root", ""); 
+    mysql_select_db("clicker") or die(mysql_error()); 
+	session_set_cookie_params(3156000);
+	session_save_path("../sessions");
+?>
